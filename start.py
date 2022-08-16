9943adddf0e3a302c40460ac80b6e0c95b102075
ananya0809 <ananya.199303010@muj.manipal.edu>
2022-08-16 17:24:38 +0530

    start: replace manual code with BotHandler class

--- start.py
+++ start.py
@@ -1,12 +1,6 @@
-import telebot
 from config import Config
-bot = telebot.TeleBot(Config.API_KEY, parse_mode=None)
+from bot_handler import BotHandler
 
-class MsgHandler:
-    @bot.message_handler(commands=['hi'])
-    def hi(message):
-        bot.send_message(message.chat.id, "Hello!")
-    @bot.message_handler(func=lambda message: True) 
-    def echo_all(message):
-	    bot.reply_to(message, message.text)
-bot.polling()
+bothandle = BotHandler(Config.API_KEY)
+bothandle.setup_bot()
+bothandle.start_polling()
