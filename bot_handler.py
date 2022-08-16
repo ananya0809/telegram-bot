72ef8c1a38543164655d52b4102d06ffb49c8562
ananya0809 <ananya.199303010@muj.manipal.edu>
2022-08-16 16:00:07 +0530

    BotHandler: add setup method for bot handlers

--- bot_handler.py
+++ bot_handler.py
@@ -3,3 +3,13 @@ import telebot
 class BotHandler:
     def __init__(self, apikey):
         self.bot = telebot.TeleBot(apikey, parse_mode=None)
+    
+    def setup_bot(self):
+        @self.bot.message_handler(commands=['hi'])
+        def hi(message):
+            self.bot.send_message(message.chat.id, "Hello!")
+        @self.bot.message_handler(func=lambda message: True) 
+        def echo_all(message):
+            self.bot.reply_to(message, message.text)
+
+
