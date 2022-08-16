dd35e5ef78714fafabc1ad27ac6bf8527bd91dee
ananya0809 <ananya.199303010@muj.manipal.edu>
2022-08-16 15:38:45 +0530

    basic: restructured code and used class based config

--- start.py
+++ start.py
@@ -1,12 +1,12 @@
-import os
 import telebot
-apikey = os.getenv('API_KEY')
-bot = telebot.TeleBot(apikey, parse_mode=None)
+from config import Config
+bot = telebot.TeleBot(Config.API_KEY, parse_mode=None)
 
-@bot.message_handler(commands=['hi'])
-def hi(message):
-    bot.send_message(message.chat.id, "Hello!")
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+class MsgHandler:
+    @bot.message_handler(commands=['hi'])
+    def hi(message):
+        bot.send_message(message.chat.id, "Hello!")
+    @bot.message_handler(func=lambda message: True) 
+    def echo_all(message):
+	    bot.reply_to(message, message.text)
 bot.polling()
