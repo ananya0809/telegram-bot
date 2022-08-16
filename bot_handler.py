fd31ba5a48e00c67e9e27c48d65656e3aeb070fd
ananya0809 <ananya.199303010@muj.manipal.edu>
2022-08-16 17:54:57 +0530

    BotHandler: add command to set and get a value

--- bot_handler.py
+++ bot_handler.py
@@ -8,9 +8,16 @@ class BotHandler:
         @self.bot.message_handler(commands=['hi'])
         def hi(message):
             self.bot.send_message(message.chat.id, "Hello!")
-        @self.bot.message_handler(func=lambda message: True) 
-        def echo_all(message):
-            self.bot.reply_to(message, message.text)
+    
+    def set_and_get(self):
+        @self.bot.message_handler(commands=['set'])
+        def set(message):
+            self.bot.reply_to(message, "OK" )
+            self.num = message.text[4:]
+            print(self.num)
+        @self.bot.message_handler(commands=['get'])
+        def get(message):
+            self.bot.reply_to(message, self.num)
     
     def start_polling(self):
         self.bot.polling()
